6b941b4f9c1bbf6d7427d8d9ff200819f7e266e7
unknown <ggamals7744@yahoo.com>
2026-02-10 20:10:23 +0200

    shop

--- LoginRegister.php
+++ LoginRegister.php
@@ -104,18 +104,52 @@ class LoginRegister extends InformationPage{
         $this->showToast($this->getModelPage()[$message]??$message, $type);
         $this->initEvent('createModel', 'createForm', $this->getLanguage(), $this->getChangeLang(), $this->getModelTitle(), $this->getModelButton(), 'lang', $this->getMyLanguage());
         $this->initEvent('style_modal', 'style_form', $this->getStyleFile(), $this->getChangeStyle(), $this->getModalTitleStyle(), $this->getModalButtonStyle(), 'style', $this->getStyle());
+        echo <<<HTML
+        <script type="text/javascript">
+            function restValue(id, style_lang){
+                closeForm(id);
+                removeClass(id);
+                if($(id).find('input[name="id"]:checked').val() !== style_lang)
+                    $(id).find('.flexCheck').prop('checked', true);
+            }
+            function changeLangStyle(el, idForm, style_lang, idModal, error){
+                validForm(idForm);
+                if(el.value !== style_lang)
+                    $(idModal).find('.flexCheck')[0].setCustomValidity('');
+                else
+                    el.setCustomValidity(error);
+            }
+            function sendLangStyle(idForm, idModel, style_lang, error){
+                validForm(idForm);
+                if($(idModel).find('input[name="id"]:checked').val() === style_lang)
+                    $(idModel).find('input[name="id"]:checked')[0].setCustomValidity(error);
+            }
+        </script>
+    HTML;
     }
     function initEvent($idModel, $idForm, $style_lang, $error, $title, $button, $state, $data){
-        $action = 'ChangeLangPost.php';
-        include('start_model.php');
-        echo '<input type="hidden" value="'.$this->getId().'" name="superId">
-        <input type="hidden"value="'.$state.'" name="state">
-        <input type="hidden" name="change_language" value="'.$this->getUrlName2().'">';
+        echo<<<HTML
+        <div class="modal fade" id="{$idModel}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="SettingLanguage">{$title}</h5>
+                    <button type="button" id="close_button" onclick="restValue('#{$idModel}', '{$style_lang}')" class="btn btn-dark">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form id="{$idForm}" action="ChangeLangPost.php" method="POST">
+                <div class="modal-body">
+                    <input type="hidden" value="{$this->getId()}" name="superId">
+                    <input type="hidden"value="{$state}" name="state">
+                    <input type="hidden" name="change_language" value="{$this->getUrlName2()}">
+
+        HTML;
         foreach ($data as $key => $value)
             if($key === $style_lang)
                 echo <<<HTML
                     <div class="form-check">
-                    <input name="id" class="form-check-input flexCheck" value="{$key}" checked type="radio">
+                    <input name="id" onchange="changeLangStyle(this, '#{$idForm}', '{$style_lang}', '#{$idModel}', '{$error}')" class="form-check-input flexCheck" value="{$key}" checked type="radio">
                     <label  class="form-check-label">
                     {$value->getName()}
                     </label>
@@ -124,33 +158,21 @@ class LoginRegister extends InformationPage{
             else
                 echo <<<HTML
                     <div class="form-check">
-                    <input name="id" class="form-check-input" value="{$key}" type="radio">
+                    <input name="id" onchange="changeLangStyle(this, '#{$idForm}', '{$style_lang}', '#{$idModel}', '{$error}')" class="form-check-input" value="{$key}" type="radio">
                     <label  class="form-check-label">
                     {$value->getName()}
                     </label>
                     </div>
                 HTML;
-        include('end_model.php');
-
-        echo <<<HTML
-            <script type="text/javascript">
-            $('#{$idModel}').find('#close_button').on('click', function() {
-                    removeClass('#{$idModel}');
-                    if($('#{$idModel}').find('input[name="id"]:checked').val() !== '{$style_lang}')
-                        $('#{$idModel}').find('.flexCheck').prop('checked', true);
-            });
-                $('#{$idModel}').find('input[name="id"]').on('change', function(){
-                    validForm('#{$idForm}');
-                    if(this.value !== '{$style_lang}')
-                        $('#{$idModel}').find('.flexCheck')[0].setCustomValidity('');
-                    else
-                        this.setCustomValidity('{$error}');
-                });
-                $('#{$idModel}').find('#click_button').on('click', function(){
-                    if($('#{$idModel}').find('input[name="id"]:checked').val() === '{$style_lang}')
-                        $('#{$idModel}').find('input[name="id"]:checked')[0].setCustomValidity('{$error}');
-                });
-            </script>
+        echo<<<HTML
+            </div>
+                <div class="modal-footer">
+                <button type="submit" id="click_button" class="btn btn-primary" onclick="sendLangStyle('#{$idForm}', '#{$idModel}', '{$style_lang}', '{$error}')">{$button}</button>
+                </div>
+            </form>
+            </div>
+            </div>
+            </div>
         HTML;
     }
     function getDbKeyLabel(){
