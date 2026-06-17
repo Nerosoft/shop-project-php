db6aa0a5f4198b970051505c12deb28a46f07340
unknown <ggamals7744@yahoo.com>
2026-06-17 23:29:31 +0300

    shop

--- BranchChangePost.php
+++ BranchChangePost.php
@@ -1,10 +1,17 @@
 <?php
 include 'auth/SessionAdmin.php';
 if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['option'])){
-ModelJson::initView('Branches', 'SuccessfullyChangeBranch', 'success', function(){
+ModelJson::initView(($_POST['option'] === 'Home' ||
+            $_POST['option'] === 'HomeCreatePost' ||
+            $_POST['option'] === 'Branches' ||
+            $_POST['option'] === 'ChangeLanguage' ||
+            $_POST['option'] === 'Users' ||
+            $_POST['option'] === 'Product' ||
+            $_POST['option'] === 'SystemLang' ||
+            $_POST['option'] === 'MyStyle'? $_POST['option']:'MyFlexTablesView'), 'SuccessfullyChangeBranch', 'success', function(){
 class BranchChangePost extends ValidationId{
     function __construct(){
-        parent::__construct('Branches');
+        parent::__construct($_POST['option']);
         $_SESSION['userId'] = $this->keyId;
     }
 }
