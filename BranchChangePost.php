a24641af7e0c229a3ff50c3406fb968502b35ec0
unknown <ggamals7744@yahoo.com>
2026-06-17 20:04:42 +0300

    shop

--- BranchChangePost.php
+++ BranchChangePost.php
@@ -1,14 +1,7 @@
 <?php
 include 'auth/SessionAdmin.php';
 if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['option'])){
-ModelJson::initView(($_POST['option'] === 'Home' ||
-            $_POST['option'] === 'HomeCreatePost' ||
-            $_POST['option'] === 'Branches' ||
-            $_POST['option'] === 'ChangeLanguage' ||
-            $_POST['option'] === 'Users' ||
-            $_POST['option'] === 'Product' ||
-            $_POST['option'] === 'SystemLang' ||
-            $_POST['option'] === 'MyStyle'? $_POST['option']:'MyFlexTablesView'), 'SuccessfullyChangeBranch', 'success', function(){
+ModelJson::initView('Branches', 'SuccessfullyChangeBranch', 'success', function(){
 class BranchChangePost extends ValidationId{
     function __construct(){
         parent::__construct('Branches');
--- ChangeLanguagePost.php
+++ ChangeLanguagePost.php
@@ -3,18 +3,11 @@ include 'auth/SessionAdmin.php';
 if($_SERVER["REQUEST_METHOD"] !== "POST" || !isset($_POST['option']) || !isset($_POST['state']) || isset($_POST['state']) && $_POST['state'] !== 'Style' && $_POST['state'] !== 'AllNamesLanguage')
     header('LOCATION:index');
 else
-    ModelJson::initView(($_POST['option'] === 'Home' ||
-            $_POST['option'] === 'HomeCreatePost' ||
-            $_POST['option'] === 'Branches' ||
-            $_POST['option'] === 'ChangeLanguage' ||
-            $_POST['option'] === 'Users' ||
-            $_POST['option'] === 'Product' ||
-            $_POST['option'] === 'SystemLang' ||
-            $_POST['option'] === 'MyStyle'? $_POST['option']:'MyFlexTablesView'), $_POST['state'] === 'Style'?'MessageStyleLang2':'MessageStyleLang', 'success', function(){
+    ModelJson::initView(($_POST['state'] === 'AllNamesLanguage'? 'ChangeLanguage':'MyStyle'), $_POST['state'] === 'Style'?'MessageStyleLang2':'MessageStyleLang', 'success', function(){
 
     class ChangeLanguagePost extends ValidationId{
         function __construct(){
-            parent::__construct(($_POST['option']), function($myFile){
+            parent::__construct(($_POST['state'] === 'AllNamesLanguage'? 'ChangeLanguage':'MyStyle'), function($myFile){
                 return $this->changeLangStylePost($myFile);
             }, 'MessageStyleLang');
             $this->saveModel($this->changeLangStylePost($this->getObj()));
