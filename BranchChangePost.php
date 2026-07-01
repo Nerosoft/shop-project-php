d63b83292fc316d11ee2b5aeae4f3cc1e59fa5b8
unknown <ggamals7744@yahoo.com>
2026-07-01 04:09:47 +0300

    shop

--- BranchChangePost.php
+++ BranchChangePost.php
@@ -2,7 +2,7 @@
 include 'auth/SessionPost.php';
 class BranchChangePost extends ValidationId{
     function __construct(){
-        parent::__construct(preg_match('/MyFlexTables/', ModelJson::getBackPage())?explode('=', ModelJson::getBackPage())[1]:ModelJson::getBackPage());
+        parent::__construct(preg_match('/SystemLang/', ModelJson::getBackPage())?'SystemLang':(preg_match('/MyFlexTables/', ModelJson::getBackPage())?explode('=', ModelJson::getBackPage())[1]:ModelJson::getBackPage()));
         $_SESSION['userId'] = $this->keyId;
         $this->showMessage($this->getModelPage()['SuccessfullyChangeBranch']);
     }
