cd9a87ed784528a1f5f2bc17e1d4f4beeb0529b7
unknown <ggamals7744@yahoo.com>
2026-07-05 20:51:43 +0300

    shop

--- auth/SessionView.php
+++ auth/SessionView.php
@@ -3,7 +3,7 @@ include 'auth/SessionAdmin.php';
 require 'controller/InformationPage.php';
 if(ModelJson::getFileName() === 'Login' || ModelJson::getFileName() === 'Register')
     require 'controller/LoginRegister.php';
-else if(ModelJson::getFileName() !== 'Site')
+else if(ModelJson::getFileName() !== 'Site' || ModelJson::getFileName() !== 'Login' || ModelJson::getFileName() !== 'Register')
     $count = 1;
 require 'controller/AdminMenu.php';
 require 'controller/'.ModelJson::getFileName().'.php';
