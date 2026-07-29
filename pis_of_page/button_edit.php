22daacb748286d09581e2ea3b7b087e81a50e299
unknown <ggamals7744@yahoo.com>
2026-07-29 16:42:49 +0300

    shop

--- pis_of_page/button_edit.php
+++ pis_of_page/button_edit.php
@@ -14,18 +14,19 @@ if($this->getUrlName2() === 'Branches' || $this->getUrlName2() === 'MyStyle' || 
     include('all_modal/modal_changelanguage_changestyle.php');
 }
 
+$myIndexEdit = 'editModel'.($index??$this->getCount());
 if($this->getUrlName2() === 'Users' || $this->getUrlName2() === 'Product' || isset($this->getModel2()['MyFlexTables'][$this->getUrlName2()]) )
-    $this->makeCreateModal($this->getScreenModelEdit(), $this->getButtonModelEdit(), "editModel".$index, $index, $myObject);
- 
+    $this->makeCreateModal($this->getScreenModelEdit(), $this->getButtonModelEdit(), $myIndexEdit, $index, $myObject);
+$myIndexEdit = '#'.$myIndexEdit;
  
 $valueObj =  htmlspecialchars($myValue??(is_array($myObject)?json_encode($myObject):$myObject->getObj()), ENT_QUOTES, "UTF-8");
-$myIndexEdit = $index??$this->getCount();
+
 echo <<<HTML
-    <i class="fa fa-sliders fa-2x pointer" onclick="restValue('#editModel{$myIndexEdit}', '$valueObj');
+    <i class="fa fa-sliders fa-2x pointer" onclick="restValue('{$myIndexEdit}', '$valueObj');
 HTML;
 //echo html
 echo ($this->getUrlName2() === 'Product' || isset($this->getModel2()['MyFlexTables'][$this->getUrlName2()]))?<<<HTML
-$('#editModel{$index}').find('form').find('img').attr('src', './asset/product/{$this->getId()}/{$index}')"></i>
+$('{$myIndexEdit}').find('form').find('img').attr('src', './asset/product/{$this->getId()}/{$index}')"></i>
     <i class="fa fa-binoculars fa-2x pointer" onclick="openForm('#imgmodal{$index}')"></i>
     <div class="modal fade" id="imgmodal{$index}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
@@ -42,9 +43,7 @@ $('#editModel{$index}').find('form').find('img').attr('src', './asset/product/{$
             </div>
         </div>
     </div>
-HTML:<<<HTML
-    "></i>
-HTML;
+HTML:'"></i>';
 
 ?>
 </td></tr>
