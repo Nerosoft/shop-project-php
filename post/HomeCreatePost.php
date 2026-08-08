656e4fa33f42ffef8ce4cd78e534795c25f15bca
unknown <ggamals7744@yahoo.com>
2026-08-08 20:15:47 +0300

    template method pattern

--- post/HomeCreatePost.php
+++ post/HomeCreatePost.php
@@ -6,6 +6,7 @@
     }
     function getView(){
         $this->saveModel($this->saveFelxTable($this->getModel2()['AllNamesLanguage'], $this->getObj()));
+        $this->showMessage('MyFlexTables?id='.$this->keyId);
     }
 }
 $view = new HomeCreatePost();
