bd37d185287579124718ff6b17ef9f25a239b461
unknown <ggamals7744@yahoo.com>
2026-08-12 07:33:39 +0300

    template method pattern

--- pis_of_page/button_create.php
+++
@@ -1,4 +0,0 @@
-<button onclick="openForm('#createModel')" class="btn btn-primary">
-<?php
-echo $this->getModelPage()['ButtonModelCreate'].'</button>';
-$this->makeCreateModal($this->getModelPage()['ScreenModelCreate'], $this->getModelPage()['ButtonModelAdd']);
