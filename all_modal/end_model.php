eae1d5a136dc41d094a7cb89d657fd10959b684e
unknown <ggamals7744@yahoo.com>
2026-08-19 02:01:58 +0300

    template method pattern file

--- all_modal/end_model.php
+++ all_modal/end_model.php
@@ -51,7 +51,7 @@ else if(isset($index) && $index !== null && !isset($state))
     include('my_id.php');
 else if(isset($idModel) && $idModel === 'lang_modal')
     echo '<div id="allbranchbox"></div>';
-else if(isset($_SESSION['userId']) && !preg_match('/BranchChangePost/', $action) && count($this->getBranch2()) >= 1 && $this->getUrlName2() !== 'Branches' || isset($_SESSION['userId']) && !preg_match('/BranchChangePost/', $action) && count($this->getBranch2()) >= 1 && $this->getUrlName2() === 'Branches' && isset($state))
+else if(isset($_SESSION['userId']) && !preg_match('/BranchChangePost/', $action) && count($this->getBranch2()) >= 1 && $this->getUrlName2() !== 'Branches' || isset($_SESSION['userId']) && count($this->getBranch2()) >= 1 && isset($state) && $state === 'Style')
     include 'AllBranchOptionChose.php';
 ?>
 </div>
